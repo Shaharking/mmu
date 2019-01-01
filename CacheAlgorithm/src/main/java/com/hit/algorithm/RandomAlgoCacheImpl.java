2b2da9e8a2c2751174ad741c14e16399e84b929b
Tal <tal.kaptsan@playbuzz.com>
2019-01-01 10:09:01 +0200

    feat(LRUAlgoCacheImple): implement lru paging algorithm

--- CacheAlgorithm/src/main/java/com/hit/algorithm/RandomAlgoCacheImpl.java
+++ CacheAlgorithm/src/main/java/com/hit/algorithm/RandomAlgoCacheImpl.java
@@ -3,33 +3,27 @@ package main.java.com.hit.algorithm;
 import java.util.LinkedHashMap;
 import java.util.Random;
 
-public class RandomAlgoCacheImpl<K,V> extends AbstractAlgoCache<K,V>
-{
-    protected LinkedHashMap<K,V> m_Cache;
+public class RandomAlgoCacheImpl<K, V> extends AbstractAlgoCache<K, V> {
+    protected LinkedHashMap<K, V> m_Cache;
     private Random m_Random;
 
-    public RandomAlgoCacheImpl (int capacity)
-    {
+    public RandomAlgoCacheImpl(int capacity) {
         super(capacity);
-        m_Cache = new LinkedHashMap<K,V>();
+        m_Cache = new LinkedHashMap<K, V>();
         m_Random = new Random();
     }
 
     @Override
-    public V getElement(K key)
-    {
-        if (m_Cache.containsKey(key))
-        {
+    public V getElement(K key) {
+        if (m_Cache.containsKey(key)) {
             return m_Cache.get(key);
         }
         return null;
     }
 
     @Override
-    public void removeElement(K key)
-    {
-        if (m_Cache.containsKey(key))
-        {
+    public void removeElement(K key) {
+        if (m_Cache.containsKey(key)) {
             m_Cache.remove(key);
         }
     }
@@ -38,13 +32,11 @@ public class RandomAlgoCacheImpl<K,V> extends AbstractAlgoCache<K,V>
     public V putElement(K key, V value) {
 
         V returnedValue = null;
-        if (!m_Cache.containsKey(key))
-        {
-            if(m_Cache.size() == capacity)
-            {
+        if (!m_Cache.containsKey(key)) {
+            if (m_Cache.size() == capacity) {
                 returnedValue = removeElementByPolicy();
             }
-            m_Cache.put(key,value);
+            m_Cache.put(key, value);
         }
         return returnedValue;
 
@@ -56,10 +48,8 @@ public class RandomAlgoCacheImpl<K,V> extends AbstractAlgoCache<K,V>
 
         int item = m_Random.nextInt(capacity);
         int loop = 0;
-        for (K keyLoop : m_Cache.keySet())
-        {
-            if (loop == item)
-            {
+        for (K keyLoop : m_Cache.keySet()) {
+            if (loop == item) {
                 selectedKeyToDelete = keyLoop;
             }
             loop++;
--- CacheAlgorithm/src/main/java/com/hit/algorithm/SecondChance.java
+++ CacheAlgorithm/src/main/java/com/hit/algorithm/SecondChance.java
@@ -4,21 +4,19 @@ import java.util.LinkedHashMap;
 
 public class SecondChance<K, V> extends AbstractAlgoCache<K, V> {
 
-    protected LinkedHashMap<K,V> m_Cache;
+    protected LinkedHashMap<K, V> m_Cache;
     protected LinkedHashMap<K, Boolean> m_ReferenceBit;
     protected int capcity;
 
     public SecondChance(int capacity) {
         super(capacity);
-        m_Cache = new LinkedHashMap<K,V>();
+        m_Cache = new LinkedHashMap<K, V>();
         m_ReferenceBit = new LinkedHashMap<K, Boolean>();
     }
 
     @Override
-    public V getElement(K key)
-    {
-        if (m_Cache.containsKey(key))
-        {
+    public V getElement(K key) {
+        if (m_Cache.containsKey(key)) {
             m_ReferenceBit.put(key, true);
             return m_Cache.get(key);
         }
@@ -26,22 +24,18 @@ public class SecondChance<K, V> extends AbstractAlgoCache<K, V> {
     }
 
     @Override
-    public void removeElement(K key)
-    {
-        if (m_Cache.containsKey(key))
-        {
-           m_Cache.remove(key);
-           m_ReferenceBit.remove(key);
+    public void removeElement(K key) {
+        if (m_Cache.containsKey(key)) {
+            m_Cache.remove(key);
+            m_ReferenceBit.remove(key);
         }
     }
 
     @Override
     public V putElement(K key, V value) {
         V returnedValueElement = null;
-        if (! m_Cache.containsKey(key))
-        {
-            if (m_Cache.size() == capcity)
-            {
+        if (!m_Cache.containsKey(key)) {
+            if (m_Cache.size() == capcity) {
                 returnedValueElement = removeElementByPolicy();
             }
             m_Cache.put(key, value);
@@ -50,20 +44,15 @@ public class SecondChance<K, V> extends AbstractAlgoCache<K, V> {
         return returnedValueElement;
     }
 
-    private V removeElementByPolicy()
-    {
+    private V removeElementByPolicy() {
         K elementToRemove = m_Cache.entrySet().iterator().next().getKey();
         V value = null;
 
-        for(K key: m_Cache.keySet())
-        {
-            if (m_ReferenceBit.get(key) == false)
-            {
+        for (K key : m_Cache.keySet()) {
+            if (m_ReferenceBit.get(key) == false) {
                 elementToRemove = key;
                 break;
-            }
-            else
-            {
+            } else {
                 m_ReferenceBit.put(key, false);
             }
         }
